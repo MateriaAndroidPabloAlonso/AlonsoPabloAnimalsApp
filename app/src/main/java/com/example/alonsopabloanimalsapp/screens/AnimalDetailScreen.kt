1f7f638f515dbae17aa0911ef341e286a9de3250
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-05-11 20:08:16 -0600

    Funcionalidad de AnimalDetailScreen terminada

--- app/src/main/java/com/example/alonsopabloanimalsapp/screens/AnimalDetailScreen.kt
+++ app/src/main/java/com/example/alonsopabloanimalsapp/screens/AnimalDetailScreen.kt
@@ -2,15 +2,22 @@ package com.example.alonsopabloanimalsapp.screens
 
 import android.R
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -24,6 +31,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
@@ -130,6 +138,41 @@ fun AnimalDetailScreen(
                 animal.facts.forEach { fact ->
                     FactItem(text = fact)
                     Spacer(modifier = Modifier.padding(vertical = 3.dp))
+                }
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                Text(
+                    text = "Galeria de imagenes",
+                    color = Color(0xFFAEB044),
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.padding(vertical = 6.dp))
+
+                LazyRow (
+                    horizontalArrangement = Arrangement.spacedBy(6.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    items(animal.imageGallery) { image ->
+                        Box(
+                            modifier = Modifier
+                                .width(350.dp)
+                                .height(300.dp)
+                                .clip(RoundedCornerShape(30.dp))
+                        ) {
+                            AsyncImage(
+                                model = image,
+                                contentDescription = animal.name,
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
                 }
             }
         }
